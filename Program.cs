8caccf78554ed426c5470c3b80c4a98996a1a949
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-17 15:23:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace EscapeCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "\"Vanita\"";
+            Console.WriteLine(Name);
+
+            string a = "one\ntwo\nthree";
+            Console.WriteLine(a);
+
+            string b = @"c\program\\donet\\solution";
+            Console.WriteLine(b);
+        
+                    
+        }
+    }
+}
