81a0011dbbcf6d606d42e67471825434fa911107
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-17 22:17:44 +0530

    Add files via upload

---
+++ NullAbleTyp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class NullAbleTyp
+
+{
+    static void main()
+    {
+        bool? AreYouMajor = null;
+        if (AreYouMajor == true)
+        {
+            Console.WriteLine("user is major");
+        }
+        else if (AreYouMajor == false)
+        {
+            Console.WriteLine("user is not a major");
+
+        }
+        else
+        {
+            Console.WriteLine("user did not answer the quuestion");
+        }
+        }
+    
+}
