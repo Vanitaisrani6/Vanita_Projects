f8ef02873f465a81dfd687acbf40f18e95ec230c
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-17 22:11:28 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,48 @@
 ﻿using System;
 
-namespace Array
+namespace Casting
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] evenNumbers = new int[4];
-            evenNumbers[0] = 12;
-            evenNumbers[1] = 13;
-            evenNumbers[2] = 14;
-            evenNumbers[3] = 15;
-            Console.WriteLine(evenNumbers[2]);
+            //implicit casting
+            int i = 3;
+            float f = i;
+            Console.WriteLine("Implicit Casting Result  {0}", f);
+            
+            // explicit casing using covert method
+            int j = 100;
+            float k = Convert.ToInt32(i);
+            Console.WriteLine("Explicit Casting result  {0}",k);
+
+
+            //explicit casting using type cast operator (if thw integer value is less then the converted value than the conversion is done using this operator)
+            float h = 12356789.88F;
+            int m = (int)h;
+            Console.WriteLine("Type cast operator Result  {0}",m);
+
+            // Parse and TryParse
+            string StrNo = "100";
+            int e = int.Parse(StrNo);
+            Console.WriteLine("parse result  {0}",i);
+
+            // exception when the string has 100TG i.e TG so we use tryParse
+            int result = 0;
+
+            string StrNo1 = "100";
+           bool IsConvertedSuccess= int.TryParse(StrNo1, out result);
+
+            if (IsConvertedSuccess)
+            {
+                Console.WriteLine(result);
+
+            }
+            else
+            {
+                Console.WriteLine("Plese enter a valid no");
+            }
+
         }
     }
 }
