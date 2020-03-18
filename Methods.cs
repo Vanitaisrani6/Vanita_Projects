e006327762cb284cec86f356daf56b4ec4f4d646
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-18 16:55:26 +0530

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EvenNos
+{
+    class Methods
+    {
+        public static void  EvenNo(int Target)
+        {
+            int Start = 0;
+            while (Start <= Target)
+            {
+                Console.WriteLine(Start);
+                Start = Start + 2;
+            }
+        }
+        public int Add(int FN, int SN)
+        {
+            return FN + SN;
+        }
+
+        static void Main(string[] args)
+        {
+            Methods.EvenNo(30);
+            Methods h = new Methods();
+            int sum=h.Add(10, 20);
+            
+            Console.WriteLine("Addition Result  {0}",sum);
+
+
+        }
+    }
+}
