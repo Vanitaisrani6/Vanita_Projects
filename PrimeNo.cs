5ee2d18f19a6060d5bfa7b96b71ef7bfaee4ba0d
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-18 16:56:31 +0530

    Add files via upload

---
+++ PrimeNo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loops
+{
+    class PrimeNo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the No to be checked");
+            int n = int.Parse(Console.ReadLine());
+           
+            if ((n == 0) || (n == 1))
+            {
+                Console.WriteLine("one and zero are not prime no");
+
+            }
+           
+            else if (n % 10 == 0)
+            {
+                Console.WriteLine("The no. is not a prime no.");
+
+            }
+            else
+            {
+                Console.WriteLine("The no is prime no");
+            }
+        }
+    }
+}
