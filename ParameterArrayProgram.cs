472f4293081bfabdccde25fd7a8ed625dd4415e6
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-18 16:57:59 +0530

    Add files via upload

---
+++ ParameterArrayProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParameterArray
+{
+    class ParameterArrayProgram
+    {
+        public static void ParamsMethod(params int[] Numbers)//param keyword makes the array without passing paramerters in main method
+        {
+            //for parameter array we use paras keyword
+            Console.WriteLine("There are {0} elements", Numbers.Length);
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+
+            int[] Numbers = new int[3];
+            Numbers[0] = 12;
+            Numbers[1] = 300;
+            Numbers[2] = 100;
+
+            // ParamsMethod();// output will be no element in array
+            ParamsMethod(Numbers);
+
+        }
+    }
+}
+
