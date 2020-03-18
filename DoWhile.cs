faf9e2c763a966257d6f10432826e185a0f79e72
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-18 16:54:00 +0530

    Add files via upload

---
+++ DoWhile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DoWhileloop
+{
+    class DoWhile
+    {
+        static void Main(string[] args)
+        {
+            string UserChoice = "";
+            do
+            {
+                Console.WriteLine("plz enter your target?");
+                int UserTarget = int.Parse(Console.ReadLine());
+
+                int start = 0;
+                while (start <= UserTarget)
+                {
+                    Console.Write(start + " ");
+                    start = start + 2;
+
+                }
+
+                do
+                {
+                    Console.WriteLine("Do you want to contine- Yes or No");
+                    UserChoice = Console.ReadLine().ToUpper();
+                    if (UserChoice != "YES" && UserChoice != "NO")
+                    {
+                        Console.WriteLine("Invalid Choice please say Yes or No");
+                    }
+                } while (UserChoice != "YES" && UserChoice != "NO");
+
+            } while (UserChoice == "YES");
+
+
+        } }
+}
