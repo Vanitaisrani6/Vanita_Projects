f0ce27e4d3511fa850e9b6448794fda2cd13fc08
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-19 17:06:16 +0530

    Add files via upload

---
+++ Constructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Consumer
+    {
+    string FirstName,LaseName;
+    public Consumer(string FN,string LN)
+
+    {
+        this.FirstName = FN;
+        this.LaseName = LN;
+
+    }
+    public void PrintingFullNAme()
+    {
+        Console.WriteLine("Full Name {0}", this.FirstName + " " + this.LaseName);
+
+    }
+    ~Consumer()
+    { 
+        // clean up code
+    }
+
+}
+namespace Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Consumer c1 = new Consumer("Vanita" , "Israni");
+            c1.PrintingFullNAme();
+        }
+    }
+}
