48b342e204fdb5b9fab5bb9ebee224db9132fa22
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-27 12:51:31 +0530

    Add files via upload

---
+++ MethodHiding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Abstraction_MethodHiing_
+{
+    class Employee
+    {
+        public string FirstNAme;
+        public string LastName;
+
+        public void print()
+        {
+            Console.WriteLine(FirstNAme + " " + LastName);
+        }
+    }
+    class FullTImeEmp :Employee
+    { 
+    }
+    class PArtTimeEmp :Employee
+    {
+        public new void print()//if we want to hide the base class member the new keyword 
+        {// i.e in this case we are making an object of part time employee which in inherited by employee so it will hide print in employee and print print in PTE
+            //Console.WriteLine(FirstNAme + " " + LastName + " "+" _Student");
+            base.print();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FullTImeEmp FTE = new FullTImeEmp();
+            FTE.FirstNAme = "Vanita";
+            FTE.LastName = "ISrani";
+            FTE.print();
+
+
+
+            PArtTimeEmp PTE = new PArtTimeEmp();
+            PTE.FirstNAme = "Rohan";
+            PTE.LastName = "Israni";
+            PTE.print();
+
+        }
+    }
+}
