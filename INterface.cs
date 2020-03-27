2cb32763655d65ff3692f8611a9b831bfdf8a455
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-27 12:49:24 +0530

    Add files via upload

---
+++ INterface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Interface
+{interface ICostumer//i is used just to initialized
+    {  //interface has only decalation not implementation and it does not contain fields like int i=10;//default public// multiple inheritance can be implemented using Interface
+        void print();
+    }
+    interface I2
+    {
+        void print2();
+
+    }
+    class Costumer : ICostumer, I2
+    {
+        public void print()
+        {
+            Console.WriteLine("Interface print method");
+
+        }
+
+        public void print2()
+        {
+            Console.WriteLine("Multiple Inheritance");
+        }
+    }
+    
+    class Interface 
+    {
+        static void Main(string[] args)
+        {
+            Costumer C1= new Costumer();
+            C1.print();
+            C1.print2();
+
+        }
+    }
+}
---
+++ Interface2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Interface2
+{
+    using System;
+
+    namespace Interface
+    {
+        interface ICostumer//i is used just to initialized
+        {  //interface has only decalation not implementation and it does not contain fields like int i=10;//default public// multiple inheritance can be implemented using Interface.
+            // cannot create an instance of interface i.e cannot create object of the class and abstract classes
+            void print();
+        }
+        interface I2 : ICostumer
+        {
+            void print2();
+
+        }
+        class Costumer : I2
+        {
+            public void print()
+            {
+                Console.WriteLine("Print2");
+
+            }
+
+            public void print2()
+            {
+                Console.WriteLine("Multiple Inheritance");
+            }
+        }
+
+        class Interface
+        {
+            static void Main(string[] args)
+            {
+                Costumer C1 = new Costumer();
+                C1.print();
+                C1.print2();
+
+            }
+        }
+    }
+
+
+}
