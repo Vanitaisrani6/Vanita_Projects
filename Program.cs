0fcb8e8bf146e51e8ef1e05773213602b30d93b0
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2020-03-27 12:53:19 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-
-namespace SwitchCase
+public class Employee
 {
+    public String FirstName="FN", LastName="LN";
+
+    public void PrintFullNAme()
+    {
+        Console.WriteLine(FirstName + "  " + LastName);
+    }
+}
+
+namespace Polymorphism
+{
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the no");
-            int i = int.Parse(Console.ReadLine());
-            switch (i)
-            {
-                case 10:
-                    Console.WriteLine("your no is 10");
-                    break;
-                    case 11:
-                    Console.WriteLine("your no is 11");
-                    break;
-                default:
-                    Console.WriteLine("Invalid No");
-                    break;
+            Employee e = new Employee();
+            e.PrintFullNAme();
 
-            }
-
+           
         }
     }
 }
